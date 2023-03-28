f910b2174690733ed11bdb992521c9ea2358c6f2
Ronald <ronald@technative.nl>
2023-03-28 11:48:54 +0200

    TLDA-272: Fix handling when no policy is passed.

--- policy.tf
+++ policy.tf
@@ -1,4 +1,6 @@
 resource "aws_sns_topic_policy" "this" {
+  count = length(data.aws_iam_policy_document.this) > 0 ? 1 : 0
+
   arn = aws_sns_topic.this.arn
 
   policy = data.aws_iam_policy_document.combined.json
